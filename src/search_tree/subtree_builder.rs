97d21741427950c1f3ca17d67ac9d37ec5450c3f
Owen Page <owenjpage@zoho.com>
2025-11-12 21:46:50 +0000

    Your mum

--- src/search_tree/subtree_builder.rs
+++ src/search_tree/subtree_builder.rs
@@ -3,7 +3,10 @@ use {
         NewNode,
         UTrait,
     },
-    std::cell::RefCell,
+    std::{
+        cell::RefCell,
+        mem::MaybeUninit,
+    },
 };
 
 impl SubtreeBuilderParent for () {}
@@ -39,33 +42,51 @@ pub trait SubtreeBuilderSelfParent {}
 //     children: VecDeque<VecSubtreeBuilderChild<'this, U>>,
 // }
 
-pub trait SubtreeBuilder {
+pub trait SubtreeBuilder<R> {
     type Child;
-    fn add_child() -> Self::Child;
+    fn add_child(&self) -> Self::Child;
+
+    fn to_subtree(self) -> R;
 }
 
 pub struct SubtreeBuilderRoot<'node, U: UTrait> {
-    parent: &'node mut NewNode<U>,
+    parent:   &'node mut NewNode<U>,
     children: RefCell<Vec<SubtreeBuilderChild<'node, U>>>,
 }
 
-impl<'node, U: UTrait> SubtreeBuilder for SubtreeBuilderRoot<'node, U> {
+impl<'node, U: UTrait> SubtreeBuilder<NewNode<U>> for SubtreeBuilderRoot<'node, U> {
     type Child = &'node SubtreeBuilderChild<'node, U>;
 
-    fn add_child() -> Self::Child {
+    fn add_child(&self) -> Self::Child {
         todo!("Create child node and add to vec, return reference to child")
+    }
+
+    fn to_subtree(self) -> NewNode<U> {
+        // Convert children to nodes, add nodes to root, add to parent
+
+        todo!()
+    }
+}
+
+impl<U: UTrait> Drop for SubtreeBuilderRoot<'_, U> {
+    fn drop(&mut self) {
+        let subtree = unsafe { std::ptr::read(self) }.to_subtree();
     }
 }
 
 pub struct SubtreeBuilderChild<'root, U: UTrait> {
-    root: &'root SubtreeBuilderRoot<'root, U>,
+    root:     &'root SubtreeBuilderRoot<'root, U>,
     children: Vec<&'root Self>,
 }
 
-impl<'root, U: UTrait> SubtreeBuilder for SubtreeBuilderChild<'root, U> {
+impl<'root, U: UTrait> SubtreeBuilder<NewNode<U>> for SubtreeBuilderChild<'root, U> {
     type Child = &'root Self;
 
-    fn add_child() -> Self::Child {
+    fn add_child(&self) -> Self::Child {
         todo!("Create child node and add to parent's vec")
+    }
+
+    fn to_subtree(self) -> NewNode<U> {
+        todo!()
     }
 }
